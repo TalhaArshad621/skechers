170cb253724743873a530ca063a93e100a01252e
Talha Arshad <arshadtalha591@gmail.com>
2024-03-21 22:16:28 +0500

    Stock report dashboard

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -308,52 +308,31 @@
                 @endcomponent
             </div>
         </div> --}}
-        {{-- <div class="row">
+        <div class="row">
             <div class="@if((session('business.enable_product_expiry') != 1) && auth()->user()->can('stock_report.view')) col-sm-12 @else col-sm-6 @endif">
                 @component('components.widget', ['class' => 'box-warning'])
                   @slot('icon')
-                    <i class="fa fa-exclamation-triangle text-yellow" aria-hidden="true"></i>
+                    {{-- <i class="fa fa-exclamation-triangle text-yellow" aria-hidden="true"></i> --}}
                   @endslot
                   @slot('title')
-                    {{ __('home.product_stock_alert') }} @show_tooltip(__('tooltip.product_stock_alert'))
+                    {{ __('Stock Report') }}
                   @endslot
-                  <table class="table table-bordered table-striped" id="stock_alert_table" style="width: 100%;">
+                  <table class="table table-bordered table-striped" id="dashboard_stock_report_table" style="width: 100%;">
                     <thead>
                       <tr>
-                        <th>@lang( 'sale.product' )</th>
-                        <th>@lang( 'business.location' )</th>
-                        <th>@lang( 'report.current_stock' )</th>
+                        <th>Categories</th>
+                        <th>Available Qty</th>
+                        <th>Sold Qty</th>
+                        <th>Cost of Available Qty </th>
+                        <th>Price of Available Qty</th>
+                        <th>Cost of Sold Qty</th>
+                        <th>Price of Sold Qty</th>
                       </tr>
                     </thead>
                   </table>
                 @endcomponent
             </div>
-            @can('stock_report.view')
-                @if(session('business.enable_product_expiry') == 1)
-                  <div class="col-sm-6">
-                      @component('components.widget', ['class' => 'box-warning'])
-                          @slot('icon')
-                            <i class="fa fa-exclamation-triangle text-yellow" aria-hidden="true"></i>
-                          @endslot
-                          @slot('title')
-                            {{ __('home.stock_expiry_alert') }} @show_tooltip( __('tooltip.stock_expiry_alert', [ 'days' =>session('business.stock_expiry_alert_days', 30) ]) )
-                          @endslot
-                          <input type="hidden" id="stock_expiry_alert_days" value="{{ \Carbon::now()->addDays(session('business.stock_expiry_alert_days', 30))->format('Y-m-d') }}">
-                          <table class="table table-bordered table-striped" id="stock_expiry_alert_table">
-                            <thead>
-                              <tr>
-                                  <th>@lang('business.product')</th>
-                                  <th>@lang('business.location')</th>
-                                  <th>@lang('report.stock_left')</th>
-                                  <th>@lang('product.expires_in')</th>
-                              </tr>
-                            </thead>
-                          </table>
-                      @endcomponent
-                  </div>
-                @endif
-            @endcan
-      	</div> --}}
+      	</div>
         @if(!empty($widgets['after_dashboard_reports']))
           @foreach($widgets['after_dashboard_reports'] as $widget)
             {!! $widget !!}
@@ -395,31 +374,6 @@
             $('.nav-tabs li.active').find('a[data-toggle="tab"]').trigger('shown.bs.tab');
         });
 
-        // profit_by_products_table = $('#profit_by_products_table').DataTable({
-        //         processing: true,
-        //         serverSide: true,
-        //         "ajax": {
-        //             "url": "/reports/get-profit/product",
-        //             "data": function ( d ) {
-        //                 d.start_date = $('#profit_tabs_filter_overview')
-        //                     .data('daterangepicker')
-        //                     .startDate.format('YYYY-MM-DD');
-        //                 d.end_date = $('#profit_tabs_filter_overview')
-        //                     .data('daterangepicker')
-        //                     .endDate.format('YYYY-MM-DD');
-        //             }
-        //         },
-        //         columns: [
-        //             { data: 'product', name: 'P.name'  },
-        //             { data: 'gross_profit', "searchable": false},
-        //         ],
-        //         fnDrawCallback: function(oSettings) {
-        //             var total_profit = sum_table_col($('#profit_by_products_table'), 'gross-profit');
-        //             $('#profit_by_products_table .footer_total').text(total_profit);
-
-        //             __currency_convert_recursively($('#profit_by_products_table'));
-        //         },
-        //     });
         $.ajax({
             type: "GET",
             url: "/reports/get-sell-overview",
@@ -449,213 +403,6 @@
             },
         });
 
-        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-            var target = $(e.target).attr('href');
-            if ( target == '#buy_overview') {
-                if(typeof profit_by_categories_datatable == 'undefined') {
-                    profit_by_categories_datatable =  $.ajax({
-                        type: "GET",
-                        url: "/reports/get-buy-overview",
-                        data: function ( d ){
-                            d.start_date = $('#profit_tabs_filter_overview')
-                                .data('daterangepicker')
-                                .startDate.format('YYYY-MM-DD');
-                            d.end_date = $('#profit_tabs_filter_overview')
-                                .data('daterangepicker')
-                                .endDate.format('YYYY-MM-DD');
-                            d.location_id = $('#profit_loss_location_filter').val();
-                        },
-                        success: function (response) {
-                            $("#buy-total-items").html(response.total_items);
-                            $("#buy-amount").html(__currency_trans_from_en(response.total_buy_amount));
-                            $("#total-cost-amount").html(__currency_trans_from_en(response.total_cost_amount));
-                            $("#total-purchase-amount").html(__currency_trans_from_en(response.total_purchase_amount));
-                        },
-                    });
-                } else {
-                    profit_by_categories_datatable.reload();
-                }
-            } else if (target == '#ecommerce') {
-                if(typeof profit_by_brands_datatable == 'undefined') {
-                    profit_by_brands_datatable = $.ajax({
-                        type: "GET",
-                        url: "/reports/get-ecommerce-overview",
-                        data: function ( d ){
-                            d.start_date = $('#profit_tabs_filter_overview')
-                                .data('daterangepicker')
-                                .startDate.format('YYYY-MM-DD');
-                            d.end_date = $('#profit_tabs_filter_overview')
-                                .data('daterangepicker')
-                                .endDate.format('YYYY-MM-DD');
-                        },
-                        success: function (response) {
-                           $("#total-orders").html(response.total_orders);
-                           $("#total-ecommerce-items").html(response.total_items);
-                           $("#new-orders").html(response.new_orders);
-                           $("#total-received-amount").html(__currency_trans_from_en(response.total_received_amount));
-                           $("#total-order-amount").html(__currency_trans_from_en(response.total_order_amount));
-                           $("#ecommerce-profit-loss").html(__currency_trans_from_en(response.profit_loss));
-                           $("#completed-orders").html(response.completed_orders);
-                           $("#dispatched-orders").html(response.dispatched_order);
-                           $("#exchanged-orders").html(response.exchanged_orders);
-                           $("#cancelled-items").html(response.cancelled_items);
-                           $("#ecommerce-discount-amount").html(__currency_trans_from_en(response.discount_amount));
-                           $("#cancelled-orders").html(response.cancelled_orders);
-                           $("#cancelled-orders-amount").html(__currency_trans_from_en(response.cancelled_order_amount));
-                           $("#exchanged-orders-amount").html(__currency_trans_from_en(response.exchanged_order_amount));
-                           $("#completed-orders-amount").html(__currency_trans_from_en(response.completed_order_amount));
-
-                        },
-                    });
-                } else {
-                    profit_by_brands_datatable.reload();
-                }
-            } else if (target == '#profit_by_locations') {
-                if(typeof profit_by_locations_datatable == 'undefined') {
-                    profit_by_locations_datatable = $('#profit_by_locations_table').DataTable({
-                        processing: true,
-                        serverSide: true,
-                        "ajax": {
-                            "url": "/reports/get-profit/location",
-                            "data": function ( d ) {
-                                d.start_date = $('#profit_tabs_filter_overview')
-                                    .data('daterangepicker')
-                                    .startDate.format('YYYY-MM-DD');
-                                d.end_date = $('#profit_tabs_filter_overview')
-                                    .data('daterangepicker')
-                                    .endDate.format('YYYY-MM-DD');
-                            }
-                        },
-                        columns: [
-                            { data: 'location', name: 'L.name'  },
-                            { data: 'gross_profit', "searchable": false},
-                        ],
-                        fnDrawCallback: function(oSettings) {
-                            var total_profit = sum_table_col($('#profit_by_locations_table'), 'gross-profit');
-                            $('#profit_by_locations_table .footer_total').text(total_profit);
-
-                            __currency_convert_recursively($('#profit_by_locations_table'));
-                        },
-                    });
-                } else {
-                    profit_by_locations_datatable.ajax.reload();
-                }
-            } else if (target == '#profit_by_invoice') {
-                if(typeof profit_by_invoice_datatable == 'undefined') {
-                    profit_by_invoice_datatable = $('#profit_by_invoice_table').DataTable({
-                        processing: true,
-                        serverSide: true,
-                        "ajax": {
-                            "url": "/reports/get-profit/invoice",
-                            "data": function ( d ) {
-                                d.start_date = $('#profit_tabs_filter_overview')
-                                    .data('daterangepicker')
-                                    .startDate.format('YYYY-MM-DD');
-                                d.end_date = $('#profit_tabs_filter_overview')
-                                    .data('daterangepicker')
-                                    .endDate.format('YYYY-MM-DD');
-                            }
-                        },
-                        columns: [
-                            { data: 'invoice_no', name: 'sale.invoice_no'  },
-                            { data: 'gross_profit', "searchable": false},
-                        ],
-                        fnDrawCallback: function(oSettings) {
-                            var total_profit = sum_table_col($('#profit_by_invoice_table'), 'gross-profit');
-                            $('#profit_by_invoice_table .footer_total').text(total_profit);
-
-                            __currency_convert_recursively($('#profit_by_invoice_table'));
-                        },
-                    });
-                } else {
-                    profit_by_invoice_datatable.ajax.reload();
-                }
-            } else if (target == '#profit_by_date') {
-                if(typeof profit_by_date_datatable == 'undefined') {
-                    profit_by_date_datatable = $('#profit_by_date_table').DataTable({
-                        processing: true,
-                        serverSide: true,
-                        "ajax": {
-                            "url": "/reports/get-profit/date",
-                            "data": function ( d ) {
-                                d.start_date = $('#profit_tabs_filter_overview')
-                                    .data('daterangepicker')
-                                    .startDate.format('YYYY-MM-DD');
-                                d.end_date = $('#profit_tabs_filter_overview')
-                                    .data('daterangepicker')
-                                    .endDate.format('YYYY-MM-DD');
-                            }
-                        },
-                        columns: [
-                            { data: 'transaction_date', name: 'sale.transaction_date'  },
-                            { data: 'gross_profit', "searchable": false},
-                        ],
-                        fnDrawCallback: function(oSettings) {
-                            var total_profit = sum_table_col($('#profit_by_date_table'), 'gross-profit');
-                            $('#profit_by_date_table .footer_total').text(total_profit);
-                            __currency_convert_recursively($('#profit_by_date_table'));
-                        },
-                    });
-                } else {
-                    profit_by_date_datatable.ajax.reload();
-                }
-            } else if (target == '#profit_by_customer') {
-                if(typeof profit_by_customers_table == 'undefined') {
-                    profit_by_customers_table = $('#profit_by_customer_table').DataTable({
-                        processing: true,
-                        serverSide: true,
-                        "ajax": {
-                            "url": "/reports/get-profit/customer",
-                            "data": function ( d ) {
-                                d.start_date = $('#profit_tabs_filter_overview')
-                                    .data('daterangepicker')
-                                    .startDate.format('YYYY-MM-DD');
-                                d.end_date = $('#profit_tabs_filter_overview')
-                                    .data('daterangepicker')
-                                    .endDate.format('YYYY-MM-DD');
-                            }
-                        },
-                        columns: [
-                            { data: 'customer', name: 'CU.name'  },
-                            { data: 'gross_profit', "searchable": false},
-                        ],
-                        fnDrawCallback: function(oSettings) {
-                            var total_profit = sum_table_col($('#profit_by_customer_table'), 'gross-profit');
-                            $('#profit_by_customer_table .footer_total').text(total_profit);
-                            __currency_convert_recursively($('#profit_by_customer_table'));
-                        },
-                    });
-                } else {
-                    profit_by_customers_table.ajax.reload();
-                }
-            } else if (target == '#profit_by_day') {
-                var start_date = $('#profit_tabs_filter_overview')
-                                    .data('daterangepicker')
-                                    .startDate.format('YYYY-MM-DD');
-
-                var end_date = $('#profit_tabs_filter_overview')
-                                    .data('daterangepicker')
-                                    .endDate.format('YYYY-MM-DD');
-                var url = '/reports/get-profit/day?start_date=' + start_date + '&end_date=' + end_date;
-                $.ajax({
-                        url: url,
-                        dataType: 'html',
-                        success: function(result) {
-                           $('#profit_by_day').html(result); 
-                            profit_by_days_table = $('#profit_by_day_table').DataTable({
-                                    "searching": false,
-                                    'paging': false,
-                                    'ordering': false,
-                            });
-                            var total_profit = sum_table_col($('#profit_by_day_table'), 'gross-profit');
-                           $('#profit_by_day_table .footer_total').text(total_profit);
-                            __currency_convert_recursively($('#profit_by_day_table'));
-                        },
-                    });
-            } else if (target == '#profit_by_products') {
-                profit_by_products_table.ajax.reload();
-            }
-        });
     });
 </script>
 @endsection
