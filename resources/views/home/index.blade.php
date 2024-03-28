ab367e156186b13ba486b231d9543e8d46cceb50
Talha Arshad <arshadtalha591@gmail.com>
2024-03-28 20:40:41 +0500

    Overview report settings

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -181,24 +181,24 @@
                               </thead>
                               <tbody>
                                   <tr>
-                                      <td>Return Invoices</td>
+                                      <td>Exchange Invoices</td>
                                       <td id="return-invoices">0</td>
                                   </tr>
                                   <tr>
-                                      <td>Exchange Amount</td>
-                                      <td id="return-amount">0</td>
-                                  </tr>
-                                  <tr>
-                                      <td>Return Items</td>
+                                      <td>Exchange Items</td>
                                       <td id="return-items">0</td>
+                                  </tr>
+                                  <tr class="text-white" style="background-color: #343a40 !important;">
+                                      <td style="font-weight: 700">Exchange Amount</td>
+                                      <td style="font-weight: 700" id="return-amount">0</td>
                                   </tr>
                                   <tr>
                                       <td>Total Items Sold</td>
                                       <td id="total-items-sold">0</td>
                                   </tr>
-                                  <tr>
-                                      <td>Invoice Amount</td>
-                                      <td id="invoice-amount">0</td>
+                                  <tr class="text-white" style="background-color: #343a40 !important;">
+                                      <td style="font-weight: 700">Invoice Amount</td>
+                                      <td style="font-weight: 700" id="invoice-amount">0</td>
                                   </tr>
                                   <tr>
                                       <td>Discount</td>
@@ -212,21 +212,21 @@
                                       <td>Credit Card Payment</td>
                                       <td id="card-payment">0</td>
                                   </tr>
-                                  <tr>
-                                      <td>Total Received</td>
-                                      <td id="total-received">0</td>
+                                  <tr class="text-white" style="background-color: #343a40 !important;">
+                                      <td style="font-weight: 700">Total Received</td>
+                                      <td id="total-received" style="font-weight: 700">0</td>
                                   </tr>
                                   <tr>
                                       <td>Profit / Loss</td>
                                       <td id="profit-loss">0</td>
                                   </tr>
                                   <tr>
+                                    <td>Total Gifts Items</td>
+                                    <td id="total-gift-items">0</td>
+                                </tr>
+                                  <tr>
                                       <td>Total Gifts Amount</td>
                                       <td id="total-gift-amount">0</td>
-                                  </tr>
-                                  <tr>
-                                      <td>Total Gifts Items</td>
-                                      <td id="total-gift-items">0</td>
                                   </tr>
                                   <tr>
                                       <td>GST Tax</td>
